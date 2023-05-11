1a697b84f39830c281f27b27c9e2159e2a96bf07
Dmytro Shykhov <dima220198@gmail.com>
2023-05-12 00:15:02 +0200

    finish requests for random Character

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style/style.scss';
 import App from './components/app/App';
 
-import MarvelService from './services/MarvelService';
 
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
