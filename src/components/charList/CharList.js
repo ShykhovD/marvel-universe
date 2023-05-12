d3cdfbb86b62d324e4fc58b539808775da1f32fc
Dmytro Shykhov <dima220198@gmail.com>
2023-05-12 17:55:46 +0200

    CharList  and Char info components updating

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -2,7 +2,6 @@ import { Component } from 'react';
 import Spinner from '../spinner/Spinner'
 import ErrorMessage from '../erroMessage/ErrorMessage';
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 import MarvelService from '../../services/MarvelService';
 
 class CharList extends Component {
@@ -44,7 +43,8 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
